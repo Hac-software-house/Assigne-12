f57adb57a521f16de0d9580cf54ad0506644c112
Mubashar <muhammadmubashar596@gmail.com>
2023-09-15 04:55:53 -0700

    ok

---
+++ app/page.js
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+import Github from "./(components)/github/page";
+
+function Home() {
+    return (
+        <div>
+            <Github />
+        </div>
+    );
+}
+
+export default Home;
